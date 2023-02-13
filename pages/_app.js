96c21fdc9d52d2da7fa6efeb74f4ca5a39828e9d
ShourovHasan <shourovhasan@gmail.com>
2023-02-13 16:41:34 +0600

    Header is added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Header from '@/components/Header/Header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    import('preline')
+  }, [])
+
+
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+      {/* <Footer></Footer> */}
+    </>
+  )
 }
